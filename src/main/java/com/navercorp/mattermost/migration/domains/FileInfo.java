83bec89b94e09dbebdce47128de83eae357da53f
alecisec <alec.dixon@sa.gov.au>
2017-11-16 13:50:22 +1030

    Update FileInfo.java

--- src/main/java/com/navercorp/mattermost/migration/domains/FileInfo.java
+++ src/main/java/com/navercorp/mattermost/migration/domains/FileInfo.java
@@ -37,8 +37,8 @@ public class FileInfo {
   private String PreviewPath;
   private String Name;
   private String Extension;
-  private BigInteger Size;
   private String MimeType;
+  private BigInteger Size;
   private long Width;
   private long Height;
   private boolean HasPreviewImage;
